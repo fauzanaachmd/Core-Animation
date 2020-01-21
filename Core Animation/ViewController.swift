6ed595041a939efa4b9a73e1c6f4273e0287f1a8
Fauzan Achmad <fauzan.achmad@student.umn.ac.id>
2020-01-21 14:41:31 +0700

    animate backgroundColor, cornerRadius, borderWidth, borderColor

--- Core Animation/ViewController.swift
+++ Core Animation/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var whiteView: UIView!
+    @IBOutlet weak var playBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let layer = CALayer()
+        
+        layer.frame = self.whiteView.bounds
     }
 
-
+    @IBAction func playBtnTapped(_ sender: Any) {
+        UIView.animate(withDuration: 3, animations: {
+            self.orangeView.layer.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1) // Tutorial Ganti Warna Background
+            self.orangeView.layer.cornerRadius = 100 // Bikin box nya rounded
+            self.orangeView.layer.borderWidth = 2
+            self.orangeView.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+          
+            let layer = CALayer()
+            layer.frame = self.whiteView.bounds
+            
+            self.orangeView.layer.addSublayer(layer)
+            self.orangeView.layer.contentsGravity = .bottomRight
+        })
+    }
+    
 }
-
