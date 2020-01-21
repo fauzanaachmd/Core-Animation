22b011c31752128f7b27732e393f8a81145041bd
Fauzan Achmad <fauzan.achmad@student.umn.ac.id>
2020-01-21 11:00:53 +0700

    Initial Commit

---
+++ Core Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core Animation
+//
+//  Created by Fauzan Achmad on 21/01/20.
+//  Copyright © 2020 Fauzan Achmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
